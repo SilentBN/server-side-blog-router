015b2031eaf17d6692143f4f25c6c0d83177aa62
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-03 23:09:21 -0400

    chore: Add rate limiting to API server

--- api/server.js
+++ api/server.js
@@ -3,8 +3,16 @@
 
 const express = require("express");
 const postsRouter = require("./posts/posts-router");
+const rateLimit = require("express-rate-limit"); // Add this line
 
 const server = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+server.use(limiter);
 
 server.use(express.json());
 
