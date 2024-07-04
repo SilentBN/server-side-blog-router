04b18b1646fd4757b92e4bea4414368129596676
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-03 21:37:00 -0400

    chore: Add POST and PUT endpoints for creating and updating posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,7 @@
 // implement your posts router here
 const express = require("express");
 const Posts = require("./posts-model");
+const { post } = require("../server");
 
 const router = express.Router();
 
@@ -37,6 +38,71 @@ router.get("/:id", (req, res) => {
         .status(500)
         .json({ message: "The post information could not be retrieved" });
     });
+});
+
+// POST /api/posts
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Posts.insert({ title, contents })
+      .then(({ id }) => {
+        // After inserting, fetch the full post object
+        return Posts.findById(id);
+      })
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
+});
+
+// PUT /api/posts/:id
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Posts.findById(id)
+      .then((post) => {
+        if (post) {
+          return Posts.update(id, { title, contents });
+        } else {
+          res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist" });
+        }
+      })
+      .then((updated) => {
+        if (updated) {
+          return Posts.findById(id);
+        }
+      })
+      .then((post) => {
+        if (post) {
+          res.json(post);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: "The post information could not be modified" });
+      });
+  }
 });
 
 module.exports = router;
