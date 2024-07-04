b695456342344bb8eddc9281d572646c99ed4f42
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-03 21:17:41 -0400

    chore: Update dependencies and add express to package.json, and setup postsRouter

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
 // implement your posts router here
+const express = require("express");
+const Posts = require("./posts-model");
+
+const router = express.Router();
+
+// GET /api/posts
+router.get("/", (req, res) => {
+  res.json({ message: "GET /api/posts endpoint" });
+});
+
+// Other routes will be added here
+
+module.exports = router;
